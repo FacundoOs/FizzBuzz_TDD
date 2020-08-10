20cdca4189130e9fbe9649a0b4db1ddc43dd2030
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-10 08:43:28 +0200

    fizzbuzz working

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,15 @@
 def fizz_buzz(number)
-  if number % 3 == 0
-    'fizz'
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+     'fizz'
+  elsif number % 5 == 0
+   'buzz'
   else
-    number
+    number   
   end 
+end
+
+1.upto(100) do |x|
+  puts fizz_buzz (x.to_i)
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -6,6 +6,12 @@ describe 'fizz_buzz' do
     end
     it "returns 'fizz' if number is divisible by 3" do
         expect(fizz_buzz(3)).to eq 'fizz'
-      end
+    end
+    it "returns 'buzz' if number is divisible by 5" do
+        expect(fizz_buzz(5)).to eq 'buzz'
+    end
+    it "returns 'fizz' if number is divisible by 15" do
+        expect(fizz_buzz(15)).to eq 'fizzbuzz'
+    end
 end
 
